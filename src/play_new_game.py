a686caa962a14010ef453accfa2de9e0dfc85fd7
Josh Campbell <jcampbell84@gmail.com>
2017-11-18 14:05:35 -0800

    tweak welcome message

--- src/play_new_game.py
+++ src/play_new_game.py
@@ -11,12 +11,11 @@ from brain_training import QUESTIONS
 def play_new_game():
     """play new game intro and build question bank"""
     print("=====play_new_game fired...")
-    new_game_message = "Welcome to Memory Loss!  Playing is easy.  I'll give "\
-            "a brain teaser and then ask a question.  Your job is to answer "\
-            "with a simple Yes, or, No within 8 seconds. I'll keep going until "\
-            "someone tells me to stop. Invent your own scoring system and play "\
-            "with others for the most fun.  "\
-            "First question coming in... 3... 2... 1... "
+    new_game_message = "Umm, what are we... playing again?  Oh, right! Memory Loss! "\
+            "I'll give you a series of words, numbers, or letters that you have to remember. "\
+            "Then I'll ask you a question about them. Your job is to answer with either, "\
+            "Yes, or, No, within 8 seconds. I won't repeat any questions, and I'll keep going "\
+            "until someone tells me to stop. First question coming in... 3... 2... 1... "
     shuffle(QUESTIONS)
     question = choice(QUESTIONS)
     speech_output = new_game_message + question['question']
